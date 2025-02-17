b73fb21b43803c0ae963dc4dd7c253b9abac45fb
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-16 20:53:47 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from app import db
+
+class Reminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    medication = db.Column(db.String(100), nullable=False)
+    dose = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
