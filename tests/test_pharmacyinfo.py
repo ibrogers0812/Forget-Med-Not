8ed8500e8225289369e6acd7a3313c81c5c925d9
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-16 22:00:25 -0500

    Update test_pharmacyinfo.py
    git add test_pharmacyinfo.py
    git commit -m "Fix linting issues in test_pharmacyinfo.py"
    git push origin main

--- tests/test_pharmacyinfo.py
+++ tests/test_pharmacyinfo.py
@@ -1,5 +1,8 @@
 import unittest
-from pharmacyinfo import get_pharmacy_info, add_pharmacy_info, update_pharmacy_info, delete_pharmacy_info
+from pharmacyinfo import (
+    get_pharmacy_info, add_pharmacy_info, update_pharmacy_info, delete_pharmacy_info
+)
+
 
 class TestPharmacyInfo(unittest.TestCase):
 
@@ -22,11 +25,13 @@ class TestPharmacyInfo(unittest.TestCase):
             }
         ]
 
+
     def test_get_pharmacy_info(self):
         pharmacy_name = "HealthFirst Pharmacy"
         info = get_pharmacy_info(self.pharmacies, pharmacy_name)
         self.assertIsNotNone(info)
         self.assertEqual(info["name"], pharmacy_name)
+
 
     def test_add_pharmacy_info(self):
         new_pharmacy = {
@@ -47,6 +52,7 @@ class TestPharmacyInfo(unittest.TestCase):
         add_pharmacy_info(self.pharmacies, new_pharmacy)
         self.assertIn(new_pharmacy, self.pharmacies)
 
+
     def test_update_pharmacy_info(self):
         pharmacy_name = "HealthFirst Pharmacy"
         new_info = {
@@ -56,11 +62,13 @@ class TestPharmacyInfo(unittest.TestCase):
         updated_info = get_pharmacy_info(self.pharmacies, pharmacy_name)
         self.assertEqual(updated_info["phone"], "+1-555-000-0000")
 
+
     def test_delete_pharmacy_info(self):
         pharmacy_name = "HealthFirst Pharmacy"
         delete_pharmacy_info(self.pharmacies, pharmacy_name)
         info = get_pharmacy_info(self.pharmacies, pharmacy_name)
         self.assertIsNone(info)
+
 
 if __name__ == '__main__':
     unittest.main()
