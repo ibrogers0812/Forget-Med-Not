62fe92065c53b6f76e19e09e1496aef54439a36e
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-17 19:42:23 -0500

    Update __init__.py
    Changed the long line in tests/__init__.py for better readability due to lint issue stating it was to long.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 # Add the app directory to the sys.path to ensure proper imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
 
 # Optionally, any other setup code for your tests can go here
