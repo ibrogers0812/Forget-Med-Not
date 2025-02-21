39e320e4e47842914ff0ba1cc6141c1daf39e333
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-21 16:19:47 -0500

    Update medications.py
    Adding Spacing due to Linting errors

--- medications.py
+++ medications.py
@@ -9,14 +9,18 @@ class Medication:
     def __repr__(self):
         return f"{self.name} ({self.dose})"
 
+
 def add_medication(med_list, medication):
     med_list.append(medication)
+
 
 def remove_medication(med_list, medication_name):
     med_list = [med for med in med_list if med.name != medication_name]
 
+
 def list_medications(med_list):
     return med_list
+
 
 def find_medication(med_list, medication_name):
     for med in med_list:
