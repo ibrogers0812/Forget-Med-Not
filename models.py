124786382a4b16e8b01b7fb6381274c072d8297f
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-21 16:07:39 -0500

    Update models.py
    Updating for Flask-Login integration for User Authentication purposes

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from datetime import datetime
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
+db = SQLAlchemy()
 
-class Reminder(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    medication = db.Column(db.String(100), nullable=False)
-    dose = db.Column(db.String(50), nullable=False)
-    time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
