4cc1fad79bf667c607ec95e172526339fa3c8713
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-21 16:04:11 -0500

    Update main.py
    Updating for main blueprint and for protected routes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from app import create_app
+from flask import Blueprint, render_template
+from flask_login import login_required
 
-app = create_app()
+main = Blueprint('main', __name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
