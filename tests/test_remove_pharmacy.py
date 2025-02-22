9162699fb554dba75e0b052ad939089892b37e97
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-22 11:27:27 -0500

    Update test_remove_pharmacy.py
    Fixing build

--- tests/test_remove_pharmacy.py
+++ tests/test_remove_pharmacy.py
@@ -3,7 +3,6 @@ import pytest
 from flask import Flask
 from route import main  # Import the Flask blueprint
 
-
 @pytest.fixture
 def client():
     """Set up a test client for the Flask application."""
@@ -11,7 +10,6 @@ def client():
     app.register_blueprint(main)
     app.config['TESTING'] = True
     return app.test_client()
-
 
 def test_remove_pharmacy(client, mocker):
     """
