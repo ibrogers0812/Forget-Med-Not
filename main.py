2acb37b0725fa3085a84d0cf092a4220e6a820d7
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-22 11:42:24 -0500

    Update main.py
    updating to smooth our flask run

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template
 from flask_login import login_required, current_user
+from create_app import create_app
 
 main = Blueprint('main', __name__)
-
 
 @main.route('/')
 def index():
@@ -13,3 +13,9 @@ def index():
 @login_required
 def dashboard():
     return render_template('dashboard.html', user=current_user)
+
+# Initialize the app using the create_app function
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
