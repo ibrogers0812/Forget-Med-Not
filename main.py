ed36ad2e9b53ceb35a70ea4bb134c286965d329d
ibrogers0812 <ibrogers0812@gmail.com>
2025-02-22 11:49:31 -0500

    Update main.py
    Fixing linting errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from create_app import create_app
 
